f85e8049734b6f0037f04b5ebf91bfbd0a9394d3
Nicola Valente <nicolavalente88@gmail.com>
2020-05-20 17:45:00 +0200

    prove

--- js/script.js
+++ js/script.js
@@ -1,44 +1,32 @@
-//myJavaScript
-
-//exercise01
-var numero = 122222222;
-var numeroStringa = numero.toString();
-var stringaNumero = parseInt(numeroStringa)
-
-//exercise02
-var primaParola = "Topolino";
-var secondaParola = "Arlecchino";
-
-var lengthPrimaParola = primaParola.length;
-var lengthSecondaParola = secondaParola.length;
-
-
-if ( lengthPrimaParola > lengthSecondaParola ) {
-
-} else if ( lengthPrimaParola < lengthSecondaParola ) {
-
-} else {
-
-}
-
-//exercise03
-var numero = ("Srivi un numero");
-var numeroNumero = parseInt(numero);
-
-if ( isNaN(numeroNumero) ) {
-
-}
-
-//ercizio04
-var data = new Date();
-
-
 //esercizio5
-var = numeroUtente = prompt("Scegli un numero tra 1 e 9");
-var = numeroUtenteNumero = parseInt(numeroUtente);
+var numeroUtente = parseInt( prompt("Scegli un numero tra 1 e 9") );
+var pariodispariUtente = prompt("Pari o dispari?");
+var numeroCpu = Math.floor(Math.random() * 9) +1;
+var sommaUtenteCpu = numeroUtente + numeroCpu;
+var risultato;
+
+if( (sommaUtenteCpu % 2 ) == 0 ) {
+  risultato = "pari";
+}else {
+  risultato = "dispari";
+}
 
 console.log(numeroUtente);
-console.log(numeroUtenteNumero);
+console.log(pariodispariUtente);
+console.log(numeroCpu);
+console.log(sommaUtenteCpu);
+console.log(risultato);
+
+if(risultato == pariodispariUtente) {
+  alert("Hai azzeccato!")
+} else {
+  alert("Hai perso!")
+}
+
+
+
+
+
 
 
 
