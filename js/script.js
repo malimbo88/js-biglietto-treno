436e91384808ac1eff1ba49afa450401e61a4f39
Nicola Valente <nicolavalente88@gmail.com>
2020-05-20 16:29:25 +0200

    Prove

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//myJavaScript
+
+//exercise01
+var numero = 122222222;
+var numeroStringa = numero.toString();
+var stringaNumero = parseInt(numeroStringa)
+
+//exercise02
+var primaParola = "Topolino";
+var secondaParola = "Arlecchino";
+
+var lengthPrimaParola = primaParola.length;
+var lengthSecondaParola = secondaParola.length;
+
+
+if ( lengthPrimaParola > lengthSecondaParola ) {
+
+} else if ( lengthPrimaParola < lengthSecondaParola ) {
+
+} else {
+
+}
+
+//exercise03
+var numero = prompt("Srivi un numero");
+var numeroNumero = parseInt(numero);
+
+console.log(numero);
+console.log(numeroNumero);
